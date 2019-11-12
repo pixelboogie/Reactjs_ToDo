76bd22c176884b548cf48571e94e182dc573707a
Shane Anderson <shane@pixelboogie.com>
2019-11-12 06:40:16 -0800

    Backup

--- src/App.js
+++ src/App.js
@@ -41,6 +41,16 @@ class App extends Component {
     this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]})
   }
 
+  addTodo = (title) => {
+    // console.log(title)
+    const newTodo = {
+      id: 4,
+      title: title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo]})
+  }
+
     render() {
       // console.log("Appjs says this.state.todos: " + this.state.todos);
 
@@ -48,7 +58,7 @@ class App extends Component {
         <div className="App">
           <div className="container">
             <Header />
-            <AddTodo />
+            <AddTodo addTodo={this.addTodo} />
             <Todos todos={this.state.todos} 
             markComplete={this.markComplete}
               delTodo={this.delTodo}
