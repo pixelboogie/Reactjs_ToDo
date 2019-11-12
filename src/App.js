be860c7d29107d3c080d6ee2ba13d1cb15dd1a0d
Shane Anderson <shane@pixelboogie.com>
2019-11-12 07:40:15 -0800

    Added About and link to header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Todos from './components/Todos';
 import Header from './components/layout/Header'
 import AddTodo from './components/AddTodo'
+import About from './components/pages/About'
 import uuid from 'uuid'
 
 import './App.css';
@@ -53,19 +55,23 @@ class App extends Component {
   }
 
     render() {
-      // console.log("Appjs says this.state.todos: " + this.state.todos);
-
       return (
-        <div className="App">
-          <div className="container">
-            <Header />
-            <AddTodo addTodo={this.addTodo} />
-            <Todos todos={this.state.todos} 
-            markComplete={this.markComplete}
-              delTodo={this.delTodo}
-            />
+        <Router>
+          <div className="App">
+            <div className="container">
+              <Header />
+              <Route exact path="/" render={props => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos todos={this.state.todos} 
+                  markComplete={this.markComplete}
+                    delTodo={this.delTodo}  />
+                </React.Fragment>
+              )} />
+              <Route path="/about" component={About} />
+            </div>
           </div>
-        </div>
+        </Router>
     );
   }
 }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header(){
     return (
         <header style={headerStyle}>
             <h1>Todo List</h1>
+            <Link style={linkStyle} to="/">Home</Link> | <Link to="/about"></Link>
         </header>
     )
 
@@ -14,6 +16,11 @@ const headerStyle = {
     color: '#FF0000',
     textAlign: 'center',
     padding: '20px'
+  }
+
+  const linkStyle = {
+      color: '#fff',
+      textDecoration: 'none'
   }
 
 export default Header;
