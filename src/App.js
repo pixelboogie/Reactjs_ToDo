7a1213156934c16dc30b025fcd419e646e79f2d5
Shane Anderson <shane@pixelboogie.com>
2019-11-11 16:15:10 -0800

    Delete now works

--- src/App.js
+++ src/App.js
@@ -1,16 +1,61 @@
 import React, { Component } from 'react';
 import Todos from './components/Todos';
+import Header from './components/layout/Header'
 
 import './App.css';
 
 class App extends Component {
-  render() {
-  return (
-    <div className="App">
-     <Todos />
-    </div>
-  );
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Build for portfolio',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Apply to jobs',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Go to interviews',
+        completed: false
+      },
+    ]
+  }
+
+  markComplete = (id) => {
+    // console.log("From app.js: " + id);
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    })})
+  }
+
+  delTodo = (id) => {
+    // console.log(id)
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]})
+  }
+
+    render() {
+      // console.log("Appjs says this.state.todos: " + this.state.todos);
+
+      return (
+        <div className="App">
+  
+        <Todos todos={this.state.todos} 
+        markComplete={this.markComplete}
+        delTodo={this.delTodo}
+        />
+        </div>
+    );
+  }
 }
-}
+
+
+
 
 export default App;
---
+++ src/components/layout/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header(){
+    return (
+        <Header>
+            <h1>Todo List</h1>
+        </Header>
+    )
+
+}
+
+// const headerStyle = {
+//     background: '#333',
+//     color: '#FF0000',
+//     textAlign: 'center',
+//     padding: '20px'
+//   }
+
+export default Header;
